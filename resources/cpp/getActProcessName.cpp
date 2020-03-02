a531a26dd55947b569e8974458b63dda7e95d8c5
lixiaochun <490398311@qq.com>
2020-03-02 19:44:37 +0800

    update get pid cpp

--- resources/cpp/getActProcessName.cpp
+++ resources/cpp/getActProcessName.cpp
@@ -1,17 +1,25 @@
 #include <windows.h>
 #include <iostream>
+#include <Psapi.h>
+#pragma comment(lib, "Psapi.lib")
 
 using namespace std;
-DWORD GetActWindowPid()
+void GetActWindowPid()
 {
-    HWND hwnd = GetForegroundWindow();
-    DWORD processId = 0;
-    GetWindowThreadProcessId(hwnd, &processId);
-    return processId;
+    HWND active_window = GetForegroundWindow();
+    DWORD active_pid = 0;
+    GetWindowThreadProcessId(active_window, &active_pid);
+    // HANDLE active_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, active_pid);
+    // char active_pname[MAX_PATH] = {0};
+    // // GetProcessImageFileNameA(active_handle, active_pname, sizeof(active_pid));
+    // char strExePath[MAX_PATH] = {0};
+    // char strPath[MAX_PATH] = {0};
+    // GetModuleFileName(NULL, strPath, MAX_PATH + 1);
+    cout << active_pid << endl;
 }
 
 int main()
-{   
-    cout << GetActWindowPid() << endl;
+{
+    GetActWindowPid();
     return 0;
 }
